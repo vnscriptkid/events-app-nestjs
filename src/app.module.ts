484d55fd15c356ef880302ff45a5cd16dec05a13
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-28 23:31:14 +0700

    feat: connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventController } from './event/event.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 33066,
+      username: 'root',
+      password: '123456',
+      database: 'nest-events',
+    }),
+  ],
   controllers: [AppController, EventController],
   providers: [AppService],
 })
