2e6dc8512f59d2a66f58e524ca98d74c0ab3b729
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-28 20:22:09 +0700

    feat(event): less boilerplate code on update dto

--- src/event/dtos/update-event.dto.ts
+++ src/event/dtos/update-event.dto.ts
@@ -1,9 +1,13 @@
-export class UpdateEventDto {
-  name?: string;
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateEventDto } from './create-event.dto';
+// export class UpdateEventDto {
+//   name?: string;
 
-  description?: string;
+//   description?: string;
 
-  when?: string;
+//   when?: string;
 
-  address?: string;
-}
+//   address?: string;
+// }
+
+export class UpdateEventDto extends PartialType(CreateEventDto) {}
