8fbfb24602c389204759585f8ce26d007b535d2b
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-29 18:07:05 +0700

    chore(env): diff configs for diff envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Event } from './event/event.entity';
 import { EventModule } from './event/event.module';
+import ormConfig from './config/orm.config';
+import ormConfigProd from './config/orm.config.prod';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({}),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      synchronize: true,
-      entities: [Event],
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory:
+        process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd,
     }),
     EventModule,
   ],
---
+++ src/config/orm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Event } from '../event/event.entity';
+
+export default (): TypeOrmModuleOptions => ({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  entities: [Event],
+});
